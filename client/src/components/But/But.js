4c5a07c677aa3e21405bb38f30ee64ad531921e5
Alejandro <alejrespal@gmail.com>
2021-07-24 10:07:19 +0200

    logic and front search, fix bug in login an signUp

--- client/src/components/But/But.js
+++ client/src/components/But/But.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react'
+import './But.css'
 
 function But(props) {
     const [active, setActive] = useState(false)
     const buttonClick =(e)=>{
-        props.click(e.target.value)
+        props.click(e.target.value, active)
         setActive(!active)
     }
     return (
-        <button onClick = {buttonClick} value={props.value} className={active?'normal':'pulsado'}>{props.value}</button>
+        <button onClick = {buttonClick} value={props.value} className={!active?'normal':'pulsado'}>{props.value}</button>
     )
 }
 
