a8b340b1727ee785ee9a1d1ed1fc968751826a5c
Alejandro <alejrespal@gmail.com>
2021-07-28 00:00:56 +0200

    Nav.css Nav.js Footer.css done

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,25 +1,50 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import favorito from '../../images/favorito.svg'
+import lupa from '../../images/lupa.svg'
+import anadir from '../../images/anadir.svg'
+import usuario from '../../images/usuario.svg'
+import './Nav.css'
 
 function Nav() {
 
+    const history = useHistory()
+
+    const changePage =(e)=>{
+        switch (e.target.value) {
+            case 'Favoritos':
+                
+                break;
+            case 'Reseñas':
+                history.push('/addreviews')
+                break;
+            case 'Explorar':
+                history.push('/search');
+                break;
+        
+            default:
+
+                break;
+        }
+    }
 
     return (
-        <div>
-            <button>
-                <p>Explorar</p> 
-                <img src="" alt="volver a home" />
+        <div className='Navegacion'>
+            <button value='Explorar' onClick={changePage} className='button'>
+                <img src={lupa} alt="ir a busqueda" className='img'/>
+                <p className='p'>Explorar</p> 
             </button>
-            <button>
-                <p>Reseñas</p> 
-                <img src="" alt="volver a home" />
+            <button value='Reseñas'className='button'>
+                <img src={anadir} alt="ir a meter Reseña" className='img'/>
+                <p className='p'>Reseñas</p> 
             </button>
-            <button>
-                <p>Favoritos</p> 
-                <img src="" alt="volver a home" />
+            <button value='Favoritos'className='button'>
+                <img src={favorito} alt="ir a favoritos" className='img'/>
+                <p className='p'>Favoritos</p> 
             </button>
-            <button>
-                <p>Mi perfil</p> 
-                <img src="" alt="volver a home" />
+            <button value='Perfil'className='button'>
+                <img src={usuario} alt="ir a perfil" className='img'/>
+                <p className='p'>Mi perfil</p> 
             </button>
         </div>
     )
